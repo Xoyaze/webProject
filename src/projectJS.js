7e78f777929668a593a69c616772c5045435c813
Prabhat Baduwal <pra.baduwal3@gmail.com>
2023-04-25 22:02:56 +0545

    added the blender project to project section

--- src/projectJS.js
+++ src/projectJS.js
@@ -1,24 +1,39 @@
-
-let firstContainer = document.getElementById("first");
 let container = document.getElementsByClassName('project');
-let hText = document.getElementsByClassName("firstText");
+
+
+let firstText = document.getElementsByClassName("firstText");
+let thirdText = document.getElementsByClassName("thirdText");
+
 let buttonOne = document.getElementById("buttonOne");
+let buttonThree = document.getElementById("buttonThree");
+
 let firstImage = document.getElementById("firstProjectImage");
 
 container[0].addEventListener('mouseover',function(){
-    hText[0].style.top = '10%';
+    firstText[0].style.top = '10%';
     firstImage.style.scale = '1.05';
 })
 
+container[2].addEventListener('mouseover',function(){
+    thirdText[0].style.top = '70%';
+})
+
+
 buttonOne.onclick = function(){
-    hText[0].style.opacity = '1';
-    console.log("hello there");
+    firstText[0].style.opacity = '1';
 } 
 
+buttonThree.onclick = function(){
+    thirdText[0].style.opacity = '1';
+}
+
 container[0].addEventListener('mouseleave',function(){
-    container[0].classList.toggle("Heythere!!!");
-    console.log("It is unhovered on!");
-    hText[0].style.opacity = '0';
-    hText[0].style.top = '-100%';
+    firstText[0].style.opacity = '0';
+    firstText[0].style.top = '-100%';
     firstImage.style.scale = '1';
+})
+
+container[2].addEventListener('mouseleave',function(){
+    thirdText[0].style.opacity = '0';
+    thirdText[0].style.top = '-100%';
 })
