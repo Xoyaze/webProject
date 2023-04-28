d42b8767e6b3c9775271f3fedbd43d7a01cb2421
Prabhat Baduwal <pra.baduwal3@gmail.com>
2023-04-28 09:10:42 +0545

    added some css to make it look a little more beautiful

--- src/projectJS.js
+++ src/projectJS.js
@@ -3,21 +3,12 @@ let container = document.getElementsByClassName('project');
 
 let firstText = document.getElementsByClassName("firstText");
 let thirdText = document.getElementsByClassName("thirdText");
+let firstImage = document.getElementById("firstProjectImage");
+let secondImage = document.getElementById("secondProjectImage");
+let fourthImage = document.getElementById("fourthProjectImage");
 
 let buttonOne = document.getElementById("buttonOne");
 let buttonThree = document.getElementById("buttonThree");
-
-let firstImage = document.getElementById("firstProjectImage");
-
-container[0].addEventListener('mouseover',function(){
-    firstText[0].style.top = '10%';
-    firstImage.style.scale = '1.05';
-})
-
-container[2].addEventListener('mouseover',function(){
-    thirdText[0].style.top = '70%';
-})
-
 
 buttonOne.onclick = function(){
     firstText[0].style.opacity = '1';
@@ -27,13 +18,52 @@ buttonThree.onclick = function(){
     thirdText[0].style.opacity = '1';
 }
 
+
+container[0].addEventListener('mouseover',function(){
+    firstText[0].style.top = '10%';
+    firstImage.style.scale = '1.05';
+    firstImage.style.borderRadius = '6%';
+
+})
+
 container[0].addEventListener('mouseleave',function(){
     firstText[0].style.opacity = '0';
     firstText[0].style.top = '-100%';
     firstImage.style.scale = '1';
+    firstImage.style.borderRadius = '5%';
+})
+
+container[1].addEventListener('mouseover',function(){
+    // firstText[0].style.top = '10%';
+    secondImage.style.scale = '1.05';
+    secondImage.style.borderRadius = '6%';
+    
+})
+
+container[1].addEventListener('mouseleave',function(){
+    // firstText[0].style.opacity = '0';
+    // firstText[0].style.top = '-100%';
+    secondImage.style.scale = '1';
+    secondImage.style.borderRadius = '5%';
+})
+
+container[2].addEventListener('mouseover',function(){
+    thirdText[0].style.top = '70%';
 })
 
 container[2].addEventListener('mouseleave',function(){
     thirdText[0].style.opacity = '0';
     thirdText[0].style.top = '-100%';
+})
+
+container[3].addEventListener('mouseover',function(){
+    // firstText[0].style.top = '10%';
+    fourthImage.style.scale = '1.05';
+    fourthImage.style.borderRadius = '6%';
+})
+
+container[3].addEventListener('mouseleave',function(){
+    // firstText[0].style.opacity = '0';
+    fourthImage.style.scale = '1';
+    fourthImage.style.borderRadius = '5%';
 })
